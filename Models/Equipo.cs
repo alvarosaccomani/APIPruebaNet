97aaa1d6396174ae4d4db4028dbdf2ed9b2d2950
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-06 12:38:20 -0300

    Agrego el archivo Equipo.cs a Models

---
+++ Models/Equipo.cs
@@ -0,0 +1,9 @@
+﻿namespace APIPruebaNet.Models
+{
+    public class Equipo
+    {
+        public int eq_Id { get; set; }
+        public string eq_Nombre { get; set; }
+        public double eq_Precio { get; set; }
+    }
+}
