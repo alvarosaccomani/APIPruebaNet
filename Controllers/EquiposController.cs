6ec6baf2e444c2bad967eaee3e50a3be29e54a8f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-06 12:41:02 -0300

    Agrego el archivo EquiposController a Controllers

---
+++ Controllers/EquiposController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIPruebaNet.Models;
+using APIPruebaNet.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIPruebaNet.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        private readonly EquipoService _equipoService;
+        public EquiposController(EquipoService equipoService)
+        {
+            _equipoService = equipoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Equipo>>> GetEquipos()
+        {
+            var equipos = await _equipoService.GetEquiposAsync();
+            return Ok(equipos);
+        }
+    }
+}
