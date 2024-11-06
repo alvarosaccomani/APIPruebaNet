c87cae123299b77642051b7adece0b5136b3074f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-06 15:06:30 -0300

    Agrego el metodo GetEquipoAsync al archivo EquipoService.cs

--- Services/EquipoService.cs
+++ Services/EquipoService.cs
@@ -33,5 +33,32 @@ namespace APIPruebaNet.Services
             }
             return equipos;
         }
+
+        public async Task<Equipo> GetEquipoAsync(int eq_Id)
+        {
+            Equipo equipo = null;
+
+            using (var connection = new NpgsqlConnection(_connectionString)) 
+            {
+                await connection.OpenAsync();
+                using(var command = new NpgsqlCommand("SELECT eq_id, eq_nombre, eq_precio FROM eq_equipos WHERE eq_id = @eq_id", connection))
+                {
+                    command.Parameters.AddWithValue("eq_Id", eq_Id);
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            equipo = new Equipo
+                            {
+                                eq_Id = reader.GetInt32(0),
+                                eq_Nombre = reader.GetString(1),
+                                eq_Precio = reader.GetDouble(2)
+                            };
+                        }
+                    }
+                }
+            }
+            return equipo;
+        }
     }
 }
