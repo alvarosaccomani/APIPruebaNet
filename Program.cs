ff93e6b2b63c43697939e0e7ac5a2e577d4f4cef
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-07 16:23:20 -0300

    Agrego los CORS al proyecto

--- Program.cs
+++ Program.cs
@@ -9,11 +9,26 @@ builder.Services.AddScoped<EquipoService>();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Agregar política CORS
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("PermitirTodo", builder => 
+    { 
+        builder.WithOrigins("https://localhost:7185") // Agregar el origen de tu aplicación Blazor 
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Usar la política CORS
+app.UseCors("PermitirTodo");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
