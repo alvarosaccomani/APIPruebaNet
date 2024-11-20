770ed6ef7ec83ed999efee414f4a7a455ee0913d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-20 11:12:12 -0300

    Agrego el endpoint stream al archivo EquipoController.cs

--- Controllers/EquiposController.cs
+++ Controllers/EquiposController.cs
@@ -2,6 +2,7 @@
 using APIPruebaNet.Models;
 using APIPruebaNet.Services;
 using System.Collections.Generic;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace APIPruebaNet.Controllers
@@ -60,5 +61,35 @@ namespace APIPruebaNet.Controllers
             return NoContent();
         }
 
+        [HttpGet("stream")]
+        public async Task Get([FromQuery] string prompt)
+        {
+            Response.Headers.Add("Content-Type", "text/event-stream");
+            var channel = Channel.CreateUnbounded<string>(); 
+            _ = Task.Run(async () => 
+            {
+                await foreach (var message in GenerateResponseAsync(prompt)) 
+                { 
+                    await channel.Writer.WriteAsync($"data: {message}\n\n");
+                }
+                channel.Writer.Complete();
+            });
+            await foreach (var message in channel.Reader.ReadAllAsync())
+            {
+                await Response.WriteAsync(message);
+                await Response.Body.FlushAsync();
+            }
+        }
+        
+        private async IAsyncEnumerable<string> GenerateResponseAsync(string prompt) {
+            var messages = new List<string> {
+                "Hola! Estoy aquí para ayudarte.", "¿Cómo puedo asistirte hoy?", "Espero que estés teniendo un buen día."
+            };
+            foreach (var message in messages)
+            { 
+                await Task.Delay(1000); // Simula un retraso de procesamiento
+                yield return message;
+            }
+        }
     }
 }
